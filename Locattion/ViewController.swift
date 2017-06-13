e524c57fe3e517d39bc6c32ac258062d69827e1a
Felwa Al-meshal <felwa.ibr@gmail.com>
2017-06-14 02:17:33 +0300

    Initial Commit

---
+++ Locattion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Locattion
+//
+//  Created by Felwa Almeshal on 9/19/1438 AH.
+//  Copyright © 1438 Felwa Almeshal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
